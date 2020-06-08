d3bdf45e1e4aa395fa57d2341f21f4b245730641
Jorge Jaulis <jjaulisr@gmail.com>
2020-06-08 01:36:09 -0500

    conf(dependencies): Add  some dev dependecies

--- src/views/Heroes.js
+++ src/views/Heroes.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import Loading from '../components/Loading';
+import Loading from "../components/Loading";
 import { useDispatch, useSelector } from 'react-redux';
 import { getHeroesAction } from '../actions/HeroesAction';
 import { setToolbarTitleAction } from '../actions/ToolbarAction';
